bce2a1ffdb00d18d10c56b6b4f498f9370f492fd
Jonathan Turner <probata@hotmail.com>
2009-07-03 14:16:22 +0000

    Remove old langkit project

--- langkit/unittest.cpp
+++
@@ -1,8 +0,0 @@
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE boxedcpp_unittests
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE( add_operators )
-{
-  BOOST_CHECK_EQUAL(2, 2);
-}
