bca86c87e128cb8235b7124e63cf6806fc6d6114
Jason Turner <jason@emptycrate.com>
2012-05-16 11:54:46 -0600

    Update copyrights to 2012 #23

--- include/chaiscript/dispatchkit/proxy_functions.hpp
+++ include/chaiscript/dispatchkit/proxy_functions.hpp
@@ -1,6 +1,6 @@
 // This file is distributed under the BSD License.
 // See "license.txt" for details.
-// Copyright 2009-2011, Jonathan Turner (jonathan@emptycrate.com)
+// Copyright 2009-2012, Jonathan Turner (jonathan@emptycrate.com)
 // and Jason Turner (jason@emptycrate.com)
 // http://www.chaiscript.com
 
