6c53e08e9bf39fdb9fc69ff0c3fbac29f2417ac5
Jason Turner <jason@emptycrate.com>
2012-06-01 15:07:53 -0600

    Fix compiler warning in snow leopard

--- include/chaiscript/language/chaiscript_parser.hpp
+++ include/chaiscript/language/chaiscript_parser.hpp
@@ -1483,10 +1483,10 @@ namespace chaiscript
               while (Eol()) {}
               
               while (Case()) {
-                while (Eol());
+                while (Eol()) { } // eat
               }
 
-              while (Eol());
+              while (Eol()) { } // eat
 
               if (!Char('}')) {
                 throw exception::eval_error("Incomplete block", File_Position(m_line, m_col), *m_filename);
