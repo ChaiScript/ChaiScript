3a6caeb1c5d3d87bf2bdc7d8626f7950340cd03d
Jason Turner <jason@emptycrate.com>
2014-01-23 13:08:08 -0700

    Fix #98 for unavoidable C style returns in modules

--- src/chaiscript_stdlib.cpp
+++ src/chaiscript_stdlib.cpp
@@ -9,12 +9,21 @@
 #pragma warning(disable : 4190)
 #endif
 
+#ifdef __llvm__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
+#endif
+
 
 CHAISCRIPT_MODULE_EXPORT  chaiscript::ModulePtr create_chaiscript_module_chaiscript_stdlib()
 {
   return chaiscript::Std_Lib::library();
 }
 
+
+#ifdef __llvm__
+#pragma clang diagnostic pop
+#endif
 
 #ifdef CHAISCRIPT_MSVC
 #pragma warning(pop)
