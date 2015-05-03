74a992fad9eb8e15b2f4051bce961d5a8f1ac659
Jason Turner <jason@emptycrate.com>
2015-05-03 08:43:43 -0600

    VS2015 thread_local doesn't seem ready for primetime yet

--- include/chaiscript/chaiscript_defines.hpp
+++ include/chaiscript/chaiscript_defines.hpp
@@ -29,7 +29,7 @@
 #define CHAISCRIPT_WINDOWS
 #endif
 
-#if (defined(__GNUC__) && __GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 8) || (defined(__llvm__) && !defined(CHAISCRIPT_LIBCPP)) || (defined(CHAISCRIPT_MSVC) && !defined(CHAISCRIPT_MSVC_12)) 
+#if (defined(__GNUC__) && __GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 8) || (defined(__llvm__) && !defined(CHAISCRIPT_LIBCPP))  
 /// Currently only g++>=4.8 supports this natively
 /// \todo Make this support other compilers when possible
 #define CHAISCRIPT_HAS_THREAD_LOCAL
