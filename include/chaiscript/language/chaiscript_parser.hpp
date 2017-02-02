f5ced799cfd985b720004568d5e9830bdfefa9d1
Jason Turner <jason@emptycrate.com>
2017-02-02 08:10:47 -0800

    Hopefully find balance with gcc/clang for static_string

--- include/chaiscript/language/chaiscript_parser.hpp
+++ include/chaiscript/language/chaiscript_parser.hpp
@@ -2261,13 +2261,14 @@ namespace chaiscript
       /// Reads a unary prefixed expression from input
       bool Prefix() {
         const auto prev_stack_top = m_match_stack.size();
+        using SS = utility::Static_String;
         constexpr const std::array<utility::Static_String, 6> prefix_opers{{
-            "++", 
-            "--", 
-            "-", 
-            "+", 
-            "!", 
-            "~"
+            SS{"++"}, 
+            SS{"--"}, 
+            SS{"-"}, 
+            SS{"+"}, 
+            SS{"!"}, 
+            SS{"~"}
         }};
 
         for (const auto &oper : prefix_opers)
