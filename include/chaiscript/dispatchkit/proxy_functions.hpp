f026b8969980d003aa19e5653e182c7ee787236a
ninnghazad <ninnghazad@users.noreply.github.com>
2020-08-23 01:11:16 +0200

    ....

--- include/chaiscript/dispatchkit/proxy_functions.hpp
+++ include/chaiscript/dispatchkit/proxy_functions.hpp
@@ -80,9 +80,11 @@ namespace chaiscript
         std::vector<Boxed_Value> convert(Function_Params t_params, const Type_Conversions_State &t_conversions) const
         {
           auto vals = t_params.to_vector();
-          const auto * intid { &typeid(int) };
-          constexpr Type_Info IAMATEST(false,false,false,false,false,intid,intid);
+          
+          const std::type_info * floop { &typeid(int) };
+          constexpr Type_Info IAMATEST(false,false,false,false,false,floop,floop);
           std::cout << IAMATEST.name() << std::endl;
+          
           constexpr auto dynamic_object_type_info = user_type<Dynamic_Object>();
           for (size_t i = 0; i < vals.size(); ++i)
           {
