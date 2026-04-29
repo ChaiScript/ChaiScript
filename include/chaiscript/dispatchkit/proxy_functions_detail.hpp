b47e179900449acf076435f18538ec348bc06397
Jason Turner <jason@emptycrate.com>
2026-04-28 18:56:38 -0600

    Attempt to fix windows build for unreachable return

--- include/chaiscript/dispatchkit/proxy_functions_detail.hpp
+++ include/chaiscript/dispatchkit/proxy_functions_detail.hpp
@@ -80,7 +80,7 @@ namespace chaiscript {
       }
 
       template<typename Callable, typename Ret, typename... Params, size_t... I>
-      Ret call_func(Ret (*)(Params...),
+      Ret call_func_impl(Ret (*)(Params...),
                     std::index_sequence<I...>,
                     const Callable &f,
                     [[maybe_unused]] const chaiscript::Function_Params &params,
@@ -95,13 +95,13 @@ namespace chaiscript {
       template<typename Callable, typename Ret, typename... Params>
       Boxed_Value
       call_func(Ret (*sig)(Params...), const Callable &f, const chaiscript::Function_Params &params, const Type_Conversions_State &t_conversions) {
-        return Handle_Return<Ret>::handle(call_func(sig, std::index_sequence_for<Params...>{}, f, params, t_conversions));
+        return Handle_Return<Ret>::handle(call_func_impl(sig, std::index_sequence_for<Params...>{}, f, params, t_conversions));
       }
 
       template<typename Callable, typename... Params>
       Boxed_Value
       call_func(void (*sig)(Params...), const Callable &f, const chaiscript::Function_Params &params, const Type_Conversions_State &t_conversions) {
-        call_func(sig, std::index_sequence_for<Params...>{}, f, params, t_conversions);
+        call_func_impl(sig, std::index_sequence_for<Params...>{}, f, params, t_conversions);
         return Handle_Return<void>::handle();
       }
 
